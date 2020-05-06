302cfd9d9254e626a0c849c9b3f9d6968e3f60f8
Agusgal <agaldeman@yahoo.com.ar>
2020-05-06 01:58:41 -0300

    layout y funcionalidad basica para agregar tracks

---
+++ Frontend/MainWindow.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1000, 624)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.scrollArea = QtWidgets.QScrollArea(self.centralwidget)
+        self.scrollArea.setGeometry(QtCore.QRect(350, 10, 641, 551))
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 639, 549))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.trackwindow_layout = QtWidgets.QVBoxLayout(self.scrollAreaWidgetContents)
+        self.trackwindow_layout.setObjectName("trackwindow_layout")
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.frame = QtWidgets.QFrame(self.centralwidget)
+        self.frame.setGeometry(QtCore.QRect(10, 10, 311, 321))
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(130, 410, 113, 32))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(130, 480, 113, 32))
+        self.pushButton_2.setObjectName("pushButton_2")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1000, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "agregar track"))
+        self.pushButton_2.setText(_translate("MainWindow", "sacar track"))
---
+++ Frontend/main.py
@@ -0,0 +1,194 @@
+from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5.QtWidgets import QMessageBox
+import numpy as np
+
+from Tracks import Track
+from MainWindow import Ui_MainWindow
+from MplWidget import MplWidget
+
+import sys
+
+
+#####  This code makes exceptions appear when sometimes pyqt wraps them in some random error  #####
+sys._excepthook = sys.excepthook
+
+def my_exception_hook(exctype, value, traceback):
+    # Print the error and traceback
+    print(exctype, value, traceback)
+    # Call the normal Exception hook after
+    sys._excepthook(exctype, value, traceback)
+    sys.exit(1)
+
+sys.excepthook = my_exception_hook
+
+####################################################################################################
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    switch_window = QtCore.pyqtSignal()
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.ui = Ui_MainWindow()
+
+        self.ui.setupUi(self)
+
+        self.rows = 0
+        #esta lista contiene los marcos que a su vez tienen a la timeline y el panel de control dentro
+        self.tracks = []
+
+
+        ###   Callbacks   ###
+
+
+        self.ui.pushButton.clicked.connect(self.add_track)
+
+
+
+        ###   Callbacks   ###
+
+
+    #creates track object
+    def create_track(self):
+        pass
+
+
+    #adds track to gui
+    def add_track(self):
+
+        #track = TrackUI()
+        #track.setParent(self.ui.scrollAreaWidgetContents)
+        self.ui.trackwindow_layout.addWidget(TrackUI())
+        #self.rows += 1
+
+        #frame = QtWidgets.QFrame(self.ui.scrollAreaWidgetContents)
+        #frame.setMinimumSize(QtCore.QSize(0, 230))
+        ##frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        #frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        #frame.setObjectName("frame" + str(self.rows + 1))
+
+        #horizontallayout = QtWidgets.QHBoxLayout(frame)
+        #horizontallayout.setObjectName("horizontalLayout" + str(self.rows + 1))
+
+        #control_panel = QtWidgets.QWidget(frame)
+        #control_panel.setMinimumSize(QtCore.QSize(150, 0))
+        #control_panel.setObjectName("control_panel" + str(self.rows + 1))
+        #horizontallayout.addWidget(control_panel)
+
+        #tracktimeline = MplWidget(frame)
+        #tracktimeline.setMinimumSize(QtCore.QSize(400, 0))
+        #tracktimeline.setObjectName("tracktimeline" + str(self.rows + 1))
+        #horizontallayout.addWidget(tracktimeline)
+
+        #elf.ui.trackwindow_layout.addWidget(frame)
+        #self.tracks.append(frame)
+        lista = self.ui.scrollAreaWidgetContents.children()
+
+        print(lista)
+
+
+
+
+
+
+        #frame = QtWidgets.QFrame(self.ui.scrollAreaWidgetContents)
+        #sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        #sizePolicy.setHorizontalStretch(0)
+        #sizePolicy.setVerticalStretch(0)
+        #sizePolicy.setHeightForWidth(frame.sizePolicy().hasHeightForWidth())
+        #frame.setSizePolicy(sizePolicy)
+        #frame.setMinimumSize(QtCore.QSize(0, 200))
+        #frame.setObjectName("frame" + str(row + 1))
+        #self.ui.trackwindow_layout.addWidget(frame, row + 1, 1, 1, 1)
+
+        #tracktimeline = MplWidget(frame)
+        #sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        #sizePolicy.setHorizontalStretch(0)
+        #sizePolicy.setVerticalStretch(0)
+        #sizePolicy.setHeightForWidth(tracktimeline.sizePolicy().hasHeightForWidth())
+        #tracktimeline.setSizePolicy(sizePolicy)
+        #tracktimeline.setMinimumSize(QtCore.QSize(400, 200))
+        #tracktimeline.setObjectName("tracktimeline" + str(row + 1))
+        #self.ui.trackwindow_layout.addWidget(tracktimeline, row + 1, 1, 1, 1)
+
+
+        #trackframe = QtWidgets.QFrame(self.ui.scrollAreaWidgetContents)
+        #sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        #sizePolicy.setHorizontalStretch(0)
+        #sizePolicy.setVerticalStretch(0)
+        #sizePolicy.setHeightForWidth(trackframe.sizePolicy().hasHeightForWidth())
+        #trackframe.setSizePolicy(sizePolicy)
+        #trackframe.setMinimumSize(QtCore.QSize(150, 100))
+        #trackframe.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        #trackframe.setFrameShadow(QtWidgets.QFrame.Raised)
+        #t3rackframe.setObjectName("trackframe" + str(row + 1))
+        #self.ui.trackwindow_layout.addWidget(trackframe, row + 1, 0, 1, 1)
+
+
+
+    #removes track from gui
+    def remove_track(self):
+        pass
+
+    #plays desired track
+    def play_track(self):
+        pass
+
+    #plays all the tracks at the same time
+    def play_song(self):
+        pass
+
+    #plots track timelines
+    def plot_timelines(self):
+        pass
+
+    #Pots spectrogram
+    def plot_spectrogram(self):
+        pass
+
+
+class TrackUI(QtWidgets.QFrame):
+    def __init__(self, parent=None):
+        super(TrackUI, self).__init__(parent)
+
+        self.setMinimumSize(QtCore.QSize(0, 230))
+
+        self.control_panel = QtWidgets.QWidget()
+        self.control_panel.setMinimumSize(QtCore.QSize(150, 0))
+
+        self.tracktimeline = MplWidget()
+        self.tracktimeline.setMinimumSize(QtCore.QSize(400, 0))
+
+        layout = QtWidgets.QHBoxLayout()
+
+        layout.addWidget(self.control_panel)
+        layout.addWidget(self.tracktimeline)
+
+        self.setLayout(layout)
+
+
+
+
+
+
+
+# Controlador de ventanas, conecta señales que le dicen a las distintas ventanas cuando abrirse y cerrarse
+class Controller:
+
+    def __init__(self):
+        pass
+
+    def show_main(self):
+        self.window = MainWindow()
+        self.window.show()
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    controller = Controller()
+    controller.show_main()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
