f0dde8f2e6efac041a23e3bbfa4d5e7ba2eb9926
Victor <voh@itba.edu.ar>
2020-05-14 16:24:06 -0300

    Flange Limits on Flange2

--- Sintetizador/effects/flanger.py
+++ Sintetizador/effects/flanger.py
@@ -20,3 +20,22 @@ def flange(data, lfofreq, depth, phase_inv, M0=1, A=1):
         if index >= 0 and index < len(data):
             out[i] = data[i] + depth * data[index]
     return out
+
+def flange2(data, speed, depth, inv, fs, min_delay = 1, max_delay = 10):
+    """ Data: Input Signal
+        Speed %: f_LFO [0.1,10] Hz
+        depth: g [0,1]
+        Delay: Minimum delay (minimum 0.001)
+        inv: invertion mode"""
+    speed = 0.1*speed
+    out = data.copy()
+    Mo = min_delay/1000 * fs
+    Mw = (max_delay-min_delay)/1000 * fs
+    if inv: depth *= -1
+    for i in range(len(data)):
+        M = Mo + Mw/2 * (1 + np.sin(2*np.pi* speed/fs * i))
+        index = int(i-M)
+        if index >= 0 and index<len(data):
+            out[i] = data[i] + depth * data[index]
+
+    return out
